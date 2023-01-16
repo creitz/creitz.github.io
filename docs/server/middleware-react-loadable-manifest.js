062596fb7044c97111a888422bedb5272f182281
Charles Reitz <cgreitz@hotmail.com>
2023-01-15 21:20:46 -0800

    Create docs

---
+++ docs/server/middleware-react-loadable-manifest.js
@@ -0,0 +1,1 @@
+self.__REACT_LOADABLE_MANIFEST={"index.tsx -> ../components/Sections/Header":{id:2766,files:["static/chunks/417.3db4efafeb4880c0.js","static/chunks/766.feb374a768d597d9.js"]}}
---
+++ docs/static/ok3sgsdvQV6v9xNDNSBsU/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/658-4e439b4e4452f2f0.js","static/chunks/pages/index-0b6da711b576426c.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
